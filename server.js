760a008fec86609f91eb8fdd2e7eadfe354771ed
Alejandro Rojas <kmachappy@pm.me>
2022-06-16 16:00:30 -0700

    more boilerplating and initiating express with server listen

--- server.js
+++ server.js
@@ -3,3 +3,16 @@ const { PORT=3001 } = process.env;
 const express = require("express")
 const cors = require("cors")
 const morgan = require("morgan")
+
+
+// Initiate express
+const app = express()
+
+
+app.use(cors())
+app.use(morgan("dev"))
+app.use(express.json())
+
+
+
+app.listen(PORT, ()=>console.log(`Listening on Port ${PORT}`))
